a920f969e6793e2d8e9fee62bd1dc6d1e2b55640
Luciano Raso <luciofaso@gmail.com>
2019-08-31 15:36:20 +0100

    test button

--- scratch.py
+++ scratch.py
@@ -1,2 +1,11 @@
-# Hello World + blinking link
-print("Hello world")
+# Hello World + button
+print("system is running")
+
+import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
+GPIO.setwarnings(False) # Ignore warning for now
+GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
+GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
+
+while True: # Run forever
+    if GPIO.input(10) == GPIO.HIGH:
+        print("Button was pushed!")
