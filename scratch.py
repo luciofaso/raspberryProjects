d3aa04c9ef4cab2fc1150a0eb87048612a8f5654
Luciano Raso <luciofaso@gmail.com>
2019-08-31 16:46:53 +0100

    event-based activation

--- scratch.py
+++ scratch.py
@@ -6,6 +6,13 @@ GPIO.setwarnings(False) # Ignore warning for now
 GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(10, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 
-while True: # Run forever
-    if GPIO.input(10) == GPIO.HIGH:
-        print("Button was pushed!")
+
+def button_callback(channel):
+    print("Button was pushed!")
+
+GPIO.add_event_detect(10,GPIO.RISING,callback=button_callback)
+
+# non event-based
+# while True: # Run forever
+#    if GPIO.input(10) == GPIO.HIGH:
+#        print("Button was pushed!")
