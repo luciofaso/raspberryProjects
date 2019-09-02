adbe7ed9ea6effaa81ce206adf5681ec415f626a
Luciano Raso <luciofaso@gmail.com>
2019-09-02 20:29:41 +0100

    launch SOS

--- scratch.py
+++ scratch.py
@@ -8,14 +8,24 @@ GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
 GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # Set pin 10 to be an input pin and set initial value to be pulled low (off)
 GPIO.setup(8, GPIO.OUT, initial=GPIO.LOW)
 
+
+def launch_sos():
+    morse = {"s":"---","o":"...", " ":" "}
+    leng = {"-":1,".":0.2," ":0.0}
+    for letter in "s o s":
+        for sign in letter:
+            GPIO.output(8, GPIO.HIGH)  # Turn on
+            sleep(leng[sign])
+            GPIO.output(8, GPIO.LOW)
+            sleep(0.2)
+
 print("system has started...")
 
 
 def button_callback(channel):
-    print("Button was pushed! Led blinking")
-    GPIO.output(8, GPIO.HIGH)  # Turn on
-    sleep(1)  # Sleep for 1 second
-    GPIO.output(8, GPIO.LOW)
+    print("Button was pushed! SOS launched!")
+        launch_sos()
+
 
 GPIO.add_event_detect(7,GPIO.RISING,callback=button_callback)
 
